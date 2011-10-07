7d2c4ae00bc41602eb327b1c86388985519127e2
Thomás Sieczkowski <contato@thomashs.com.br>
2011-10-07 11:00:52 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Layla::Application.config.secret_token = '2b8b428f46cc7c11ef800f434ce75352e5758dc6754538a9772b1c511eff126c951e65b3d7d22501a93b5436b112fd182abfaf5b099fc7b2a28bbeb128576b9e'
---
+++ db/migrate/20111007134335_devise_create_advertisers.rb
@@ -0,0 +1,27 @@
+class DeviseCreateAdvertisers < ActiveRecord::Migration
+  def self.up
+    create_table(:advertisers) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+      t.timestamps
+      t.lockable
+      #
+      t.string :name
+      t.string :cnpj
+      t.string :website_url
+      t.string :xml_url
+    end
+
+    add_index :advertisers, :email,                :unique => true
+    add_index :advertisers, :reset_password_token, :unique => true
+    add_index :advertisers, :unlock_token,         :unique => true
+    # add_index :advertisers, :confirmation_token,   :unique => true
+    # add_index :advertisers, :authentication_token, :unique => true
+  end
+
+  def self.down
+    drop_table :advertisers
+  end
+end
